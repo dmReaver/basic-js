aa0433db89e8b4501a487d3659765f7e5ca2bebc
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-07 12:20:14 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,17 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(dt) {
+
+  if(dt === undefined){
+    return 'Unable to determine the time of year!'
+  }else if(dt.getMonth() >= 2 && dt.getMonth() <=4){
+    return 'spring';
+  }else if(dt.getMonth() >= 5 && dt.getMonth() <=7){
+    return 'summer';
+  }else if(dt.getMonth() >= 8 && dt.getMonth() <=10){
+    return 'autumn';
+  }else if((dt.getMonth() >= 0 && dt.getMonth() <=1)  || dt.getMonth() == 11){
+    return 'winter';
+  }else{
+    throw Error();
+  }
+
 };
