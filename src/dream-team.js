9452bd9d3074d88f223ca29c03cb07ef8fcda378
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-07 12:19:58 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let dm = [];
+  if (Array.isArray(arr)){
+    arr.forEach(e => {
+      if(typeof e == 'string'){
+        e = e.trim();
+        if(/^[A-Za-z]{1}/.test(e[0])){
+          dm.push(e[0].toUpperCase());
+        } 
+      }
+    });
+
+  } else {
+    return false;
+  }
+  return dm.sort().join('');
+
 };
+
+// console.log(createDreamTeam(['Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise']));
+
