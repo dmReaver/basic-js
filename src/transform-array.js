805c2444e8faa49d4e25bcf7869427c6d9692da2
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-07 12:20:36 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)){
+        throw Error();
+    }else{
+        if(arr.length == 0){
+            return arr
+        } else {
+            let out = [];
+            for(let i = 0; i < arr.length; i++){
+                if(arr[i] == '--discard-prev'){
+                    out.pop();
+                } else if(arr[i] == '--discard-next'){
+                    i++;
+                } else if(arr[i] == '--double-next'){
+                    out.push(arr[i+1])
+                } else if(arr[i] == '--double-prev'){
+                    out.push(arr[i-1])
+                } else{
+                    out.push(arr[i])
+                }
+            }
+            return out;
+        }
+    }
+    
 };
+
+// console.log(transform(3))
