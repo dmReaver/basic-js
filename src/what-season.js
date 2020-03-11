19950c694c50ddabd14ef4ad5dab8ddb306c5e6e
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-12 01:15:07 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,8 +1,10 @@
 module.exports = function getSeason(dt) {
-
+  console.log(dt);
   if(dt === undefined){
     return 'Unable to determine the time of year!'
-  }else if(dt.getMonth() >= 2 && dt.getMonth() <=4){
+  }else if(!dt.getTime()){
+    throw new Error("Error");
+  } else if(dt.getMonth() >= 2 && dt.getMonth() <=4){
     return 'spring';
   }else if(dt.getMonth() >= 5 && dt.getMonth() <=7){
     return 'summer';
@@ -10,8 +12,5 @@ module.exports = function getSeason(dt) {
     return 'autumn';
   }else if((dt.getMonth() >= 0 && dt.getMonth() <=1)  || dt.getMonth() == 11){
     return 'winter';
-  }else{
-    throw Error();
   }
-
 };
