77329f42ca65efea23e9d124f1aae92cf7090bf3
Salavei Dzmitry <salavei.dv@gmail.com>
2020-03-12 00:52:11 +0300

    feat: hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,8 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+    let turns_ = (2**disksNumber) -1
+    let seconds_ = turns_/(turnsSpeed/(3600));
+    return {turns: turns_, seconds: seconds_}
 }
+
+// let a = calculateHanoi(736, 78853564) //, {turns: 3.6147378671465184e+221, seconds: 1.6502813140731933e+217})
+// console.log(a);
